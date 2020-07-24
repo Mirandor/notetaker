d5a24a8166a7a14f084b034c33e541a9387179c1
Darth Mirandor <darth.mirandor@gmail.com>
2020-07-23 19:43:13 -0500

    Fixed the apiRoutes and htmlRoutes files. Updated the index.html and notes.html files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,6 @@
 var dbNotes = require("../db/db.json");
 // var notesIndex = require("../public/assets/js/index.js");
+// const store = require("../db/store");
 
 module.exports = function(app) {
 
@@ -12,11 +13,21 @@ module.exports = function(app) {
   });
 
   app.post("/api/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    var newNote = req.body;
+
+    newNote.routeName = newNote.title.replace(/\s+/g, "").toLowerCase();
+    
+    console.log(newNote);
+
+    dbNotes.push(newNote);
+
+    res.json(newNote);
   });
 
-  // app.delete("/api/notes/:id", function(req, res) {
-  //   res.json();
-  // });
+  app.delete("/api/notes/:id", function(req, res) {
+   store
+    .removeNewNote(req.params.id).then(() => res.json({ok: true}))
+    .catch((err) => res.status(500).json(err));
+  });
 
 };
--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 var express = require("express");
 var path = require("path");
 var gradient = require('gradient-string');
-// var http = require('http');
+var http = require('http');
 // var fs = require("fs");
 
 var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require("./routes/apiRoutes.js")(app);
-require("./routes/htmlRoutes.js")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
   console.log(" ");
